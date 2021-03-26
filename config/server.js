6d1e4cd21c5f81e999e63d147fee194aa5c17304
LouisLec <louis.l@obole-digitale.fr>
2021-03-26 23:50:50 +0100

    :rocket: first try with strapi, configuring for a parallel nuxt project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9b67ef976a39654c7f257f3b99194513'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '88d39820-efe3-405a-8f74-46cfa875702c'
+};
